4096e32fe7559fb6e1360deb39bea338ea83b913
Lalaine Hines <150487195+LalaineHines@users.noreply.github.com>
2025-09-13 22:54:52 +0000

    added script to setup page

---
+++ src/main.jsx
@@ -0,0 +1,43 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./style/index.css";
+
+import AboutPage from "./pages/about";
+import ContactPage from "./pages/contact";
+import ErrorPage from "./pages/error";
+import IndexPage from "./pages/index";
+import ProjectsPage from "./pages/projects";
+import RootElement from "./pages/root";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <RootElement />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "",
+                element: <IndexPage />,
+            },
+            {
+                path: "about",
+                element: <AboutPage />,
+            },
+            {
+                path: "contact",
+                element: <ContactPage />,
+            },
+            {
+                path: "projects",
+                element: <ProjectsPage />,
+            },
+        ],
+    },
+]);
+
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>
+);
