d0e495c8c88ebd2fc9dc1cad5b33bbf0a092e0fa
Lalaine Hines <150487195+LalaineHines@users.noreply.github.com>
2025-09-14 20:37:13 +0000

    added script for index page

---
+++ src/pages/index.jsx
@@ -0,0 +1,36 @@
+import { use, useState } from "react";
+import { useNavigate, useOutletContext } from "react-router-dom";
+
+import ProjectDisplay from "../partials/projectListDisplay";
+import "../style/indexPage.css";
+import "../style/projects.css";
+
+export default function IndexPage() {
+    const { pinnedProjects } = useOutletContext();
+    const navigate = useNavigate();
+
+    return (
+        <>
+        <div className="header-extension">
+            <p>
+                I'm a self-motivated full stack developer with experience in the PERN
+                stack (PostgreSQL + PrismaORM, ExpressJS, React, Node.js). I thrive in
+                flexible work environments and am interested in applying my skills
+                while continuing to learn even more about the world of web
+                development. I'm adaptable, self-driven, and eager to expand my skill
+                set with new technologies and stacks.
+            </p>
+        </div>
+        <h1>Some fo my past projects</h1>
+        <ProjectDisplay projects={pinnedProjects} className="highlights" />
+        <button
+        type="button"
+        onClick={() => {
+            navigate("/projects")
+        }}
+        >
+            See more projects
+        </button>
+        </>
+    );
+}
