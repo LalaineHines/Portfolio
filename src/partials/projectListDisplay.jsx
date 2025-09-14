d22099cc9a387f762ba29d2b16a2f229eb602392
Lalaine Hines <150487195+LalaineHines@users.noreply.github.com>
2025-09-14 20:01:59 +0000

    added script for project display list

---
+++ src/partials/projectListDisplay.jsx
@@ -0,0 +1,43 @@
+import { v4 as uuidv4 } from "uuid";
+import { Globe } from "lucide-react";
+
+import "../style/project.css";
+
+function SingleProjectDisplay({ project }) {
+    return (
+        <div>
+            <h2>{project.name}</h2>
+            <p>{project.description}</p>
+            <div className="links">
+                <a
+                href={project.livePreviewUrl}
+                target="_blank"
+                rel="noopener noreferrrer"
+                >
+                    <Globe />
+                    Website
+                </a>
+                {project.repoIfPublicElseNull ? (
+                    <a
+                    href={project.repoIfPublicElseNull}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >
+                        <ion-icon name="logo-github"></ion-icon> Project Github
+                    </a>
+                ) : null}
+            </div>
+            <img src={project.screenshotPath} alt={`${project.name} screenshot`} />
+        </div>
+    );
+}
+
+export default function ProjectDisplay({ projects, className }) {
+    return (
+        <div className={`projects ${className}`}>
+            {projects.map((project) => {
+                return <SingleProjectDisplay project={project} key={uuidv4()} />;
+            })}
+        </div>
+    );
+}
